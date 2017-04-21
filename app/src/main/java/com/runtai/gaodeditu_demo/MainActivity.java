791b87b34eb57f00619ac33e15760d22065e1a3b
836948082 <836948082@qq.com>
2017-04-21 11:25:57 +0800

    高德地图

---
+++ app/src/main/java/com/runtai/gaodeditu_demo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.runtai.gaodeditu_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
+    private TextView location;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        location = (TextView) findViewById(R.id.location);
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        location.setText(aMapLocation.getAddress());
+    }
+}
