ac0467a3dedd6695390abcf3075b88fb8f5e7266
836948082 <836948082@qq.com>
2017-04-22 08:28:53 +0800

    three

--- app/src/main/java/com/runtai/gaodeditu_demo/MainActivity.java
+++ app/src/main/java/com/runtai/gaodeditu_demo/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements AMapLocationListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         location = (TextView) findViewById(R.id.location);
+        //
     }
 
     @Override
